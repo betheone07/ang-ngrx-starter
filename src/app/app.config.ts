31afb441a231675a9d547df5a6b4045b9a6e18e3
Arnab-Mandal <Arnab.Mandal@Chubb.com>
2025-01-27 16:51:55 +0530

    feat: refactor and introduce state

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,24 @@
+import {
+  provideHttpClient,
+  withInterceptors,
+  withInterceptorsFromDi,
+} from '@angular/common/http';
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
+import { provideStore } from '@ngrx/store';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { loadingInterceptor } from './shared/services/loading-indicator.interceptor';
+import { userReducer } from './shared/store/user.reducer';
+import { provideEffects } from '@ngrx/effects';
+import { UserEffects } from './shared/store/user.effects';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideStore({ users: userReducer }),
+    provideEffects([UserEffects]),
+    provideAnimations(),
+    provideHttpClient(
+      withInterceptors([loadingInterceptor]),
+      withInterceptorsFromDi()
+    ),
+  ],
 };
